429af8a96d143d2661f1005c1aa339f908db8e3d
elitetns <tnsios92@gmail.com>
2026-08-19 16:27:52 +0700

    Delete EvoAssist directory

--- EvoAssist/make.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal EnableExtensions
-set "THEOS=/c/theos"
-set "MSYSTEM=MSYS"
-set "CHERE_INVOKING=1"
-"C:\msys64\usr\bin\bash.exe" --login -c "source /c/theos/theos-env.sh; cd \"$(cygpath -u '%CD%')\" && make %*"
